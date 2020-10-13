8cb24a61bcd3b68ff09bfb8f341f6bf4261a93c1
Silvin Lubecki <silvin.lubecki@docker.com>
2020-10-13 16:20:25 +0200

    Improve tag inspect command: - now for a manifest it fetches the config object too, to print more informations (command, entrypoint, command history on each layers, exposed ports, volumes...) - refactored the whole command - added a new format "raw" which prints exactly what was returned by the registry, "json" format is a combination of the descriptor, the config object and the manifest - added color
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- internal/commands/tag/inspect_test.go
+++ internal/commands/tag/inspect_test.go
@@ -18,16 +18,19 @@ package tag
 
 import (
 	"bytes"
-	"encoding/json"
 	"testing"
+	"time"
 
+	"github.com/docker/distribution/reference"
+	"github.com/opencontainers/image-spec/specs-go"
 	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
 	"gotest.tools/golden"
 	"gotest.tools/v3/assert"
 )
 
-func TestPrintManifest(t *testing.T) {
+func TestPrintImage(t *testing.T) {
 	manifest := ocispec.Manifest{
+		Versioned: specs.Versioned{},
 		Config: ocispec.Descriptor{
 			MediaType: "mediatype/config",
 			Digest:    "sha256:beef",
@@ -40,17 +43,64 @@ func TestPrintManifest(t *testing.T) {
 				Size:      456,
 			},
 		},
+		Annotations: map[string]string{
+			"annotation1": "value1",
+			"annotation2": "value2",
+		},
 	}
-	raw, err := json.Marshal(&manifest)
-	assert.NilError(t, err)
+	now := time.Now()
+	config := ocispec.Image{
+		Created:      &now,
+		Author:       "author",
+		Architecture: "arch",
+		OS:           "os",
+		Config: ocispec.ImageConfig{
+			User:         "user",
+			ExposedPorts: map[string]struct{}{"80/tcp": {}},
+			Env:          []string{"KEY=VALUE"},
+			Entrypoint:   []string{"./entrypoint", "parameter"},
+			Cmd:          []string{"./cmd", "parameter"},
+			Volumes:      map[string]struct{}{"/volume": {}},
+			WorkingDir:   "/workingdir",
+			Labels:       map[string]string{"label": "value"},
+			StopSignal:   "SIGTERM",
+		},
+		History: []ocispec.History{
+			// empty layer is ignored
+			{
+				Created:    &now,
+				CreatedBy:  "created-by-ignored",
+				Author:     "author-ignored",
+				Comment:    "comment-ignored",
+				EmptyLayer: true,
+			},
+			{
+				Created:    &now,
+				CreatedBy:  "/bin/sh -c #(nop) apt-get install",
+				Author:     "author-history",
+				Comment:    "comment-history",
+				EmptyLayer: false,
+			},
+		},
+	}
 	manifestDescriptor := ocispec.Descriptor{
 		MediaType: "mediatype/manifest",
 		Digest:    "sha256:abcdef",
 		Size:      789,
+		Platform: &ocispec.Platform{
+			Architecture: "arch",
+			OS:           "os",
+			OSVersion:    "osversion",
+			OSFeatures:   []string{"feature1", "feature2"},
+			Variant:      "variant",
+		},
 	}
-	out := bytes.NewBuffer(nil)
-
-	err = printManifest(raw, manifestDescriptor, "image:latest", out)
+	ref, err := reference.ParseDockerRef("image:latest")
 	assert.NilError(t, err)
-	golden.Assert(t, out.String(), "printmanifest.golden")
+	image := Image{ref, manifest, config, manifestDescriptor}
+
+	out := bytes.NewBuffer(nil)
+	err = printImage(out, &image)
+	assert.NilError(t, err)
+	golden.Assert(t, out.String(), "printimage.golden")
 }
